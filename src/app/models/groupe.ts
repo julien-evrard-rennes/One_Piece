d553d96e2dd132ecefb4d1d986849c1312d765db
julien-evrard-rennes <julien.dominique.evrard@gmail.com>
2025-11-06 11:20:55 +0100

    Injection du nombre de membres dans une fonction service

--- src/app/models/groupe.ts
+++ src/app/models/groupe.ts
@@ -3,18 +3,18 @@ import { Personnage } from "./Personnage";
 export class Groupe {
 
         id : number;
-        nom : string;
+        nom! : String;
         capitaine! : Personnage;
-        membresListe : Personnage[];
+        membresListe! : Personnage[];
+        nbmembres!: number;
 
         constructor(
         id : number,
-        nom : string,
-        membresListe : Personnage[],) {
+        nom : String,
+        nbmembres :number) {
             this.id = id;
-            //this.id = crypto.randomUUID().substring(0, 8);
             this.nom = nom;
-            this.membresListe = membresListe;
-            console.log(this);
+            //this.id = crypto.randomUUID().substring(0, 8);
+            this.nbmembres = nbmembres;
         }
     }
